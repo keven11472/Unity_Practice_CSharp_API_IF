75c3aff77d4c2cb2b0b8fd6d1d6ded046c72706c
keven11472 <67904682+keven11472@users.noreply.github.com>
2020-08-15 12:57:57 +0800

    完成走路

---
+++ CSharp_API_IF/Assets/walk.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class walk : MonoBehaviour
+{
+    public Animator ani;
+
+    /// <summary>
+    /// 走路
+    /// </summary>
+    private void Move()
+    {
+        bool w = Input.GetKey(KeyCode.W);
+
+        ani.SetBool("走路開關", w);
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+}
